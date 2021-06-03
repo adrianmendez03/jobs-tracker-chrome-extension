5b83726533affd66955f8cf448dc01b188bfdf25
Adrian Mendez <adrianmndz328@gmail.com>
2021-06-02 21:44:51 -0500

    Server is now scraping things successfully

--- src/scrape.ts
+++ src/scrape.ts
@@ -1,18 +1,37 @@
-import express from 'express'
-import puppeteer from 'puppeteer'
+import express from "express"
+import puppeteer, { Browser } from "puppeteer"
 
 export const router = express.Router()
+let browser: Browser
+;(async function () {
+  browser = await puppeteer.launch()
+})()
 
-router.post('/', async (req, res) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+router.post("/", async (req, res) => {
+  const page = await browser.newPage()
 
-    await page.goto(req.body.url, { waitUntil: 'networkidle0' })
+  await page.goto(req.body.url)
 
-    await page.screenshot({ path: "screenshot.png" })
+  const [companyName] = await page.$x(
+    '//*[@id="viewJobSSRRoot"]/div[1]/div[3]/div[1]/div[2]/div/div/div[1]/div'
+  )
 
-    await browser.close()
+  const [title] = await page.$x(
+    '//*[@id="viewJobSSRRoot"]/div[1]/div[3]/div[1]/div[1]/h1'
+  )
+  const [location] = await page.$x(
+    '//*[@id="viewJobSSRRoot"]/div[1]/div[3]/div[1]/div[2]/div/div/div[3]'
+  )
+  const [description] = await page.$x('//*[@id="jobDescriptionText"]')
 
+  const data = {
+    title: await page.evaluate((el) => el.textContent, title),
+    companyName: await page.evaluate((el) => el.textContent, companyName),
+    location: await page.evaluate((el) => el.textContent, location),
+    description: await page.evaluate((el) => el.textContent, description),
+  }
 
+  await page.close()
+
+  res.json({ ...data })
 })
-
